dfa8e2e4468e1f43ebf0659b6773809a3f59b1fc
Daniel Schöne <daniel@schoene.it>
2014-09-04 17:31:58 +0200

    fixes call to read language file

--- pi1/class.tx_abdownloads_pi1_wizicon.php
+++ pi1/class.tx_abdownloads_pi1_wizicon.php
@@ -75,7 +75,7 @@ class tx_abdownloads_pi1_wizicon {
 	 */
 	function includeLocalLang() {
 		$llFile = t3lib_extMgm::extPath( 'ab_downloads' ) . 'locallang.xml';
-		$LOCAL_LANG = t3lib_div::readLLXMLfile( $llFile, $GLOBALS['LANG']->lang );
+		$LOCAL_LANG = t3lib_div::readLLfile( $llFile, $GLOBALS['LANG']->lang );
 
 		return $LOCAL_LANG;
 	}
