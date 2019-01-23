c2ed7d1a9488682a384254bbb4de6612f9140b33
Manuel Nagel <manuel.nagelt-online.de>
2019-01-23 15:21:20 +0100

    Increase version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,18 +1,8 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "ab_downloads".
- *
- * Auto generated 16-06-2014 16:18
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array (
-	'title' => 'Modern Downloads',
-	'description' => 'This is a modern download plugin based on the Modern Linklist (ab_linklist) extension.
+$EM_CONF[$_EXTKEY] = [
+    'title'              => 'Modern Downloads',
+    'description'        => 'This is a modern download plugin based on the Modern Linklist (ab_linklist) extension.
 				
 				Please have a look at the documentation for all (additional) features available.
 
@@ -33,40 +23,39 @@ $EM_CONF[$_EXTKEY] = array (
 
 				A demo with the most recent features enabled can be found here:
 				http://typo3.andreas-bulling.de/en/demos/modern-downloads/',
-	'category' => 'plugin',
-	'shy' => 0,
-	'version' => '7.6.3',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => 'mod1',
-	'state' => 'stable',
-	'uploadfolder' => 1,
-	'createDirs' => 'uploads/tx_abdownloads/categoryImages,uploads/tx_abdownloads/downloadImages,uploads/tx_abdownloads/files',
-	'modify_tables' => 'be_groups,be_users',
-	'clearcacheonload' => 1,
-	'lockType' => '',
-	'author' => 'Andreas Bulling',
-	'author_email' => 'typo3@andreas-bulling.de',
-	'author_company' => '',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
-	'constraints' =>
-	array (
-		'depends' =>
-		array (
-			'cms' => '',
-			'static_info_tables' => '2.0.0',
-			'php' => '4.1.0-0.0.0',
-			'typo3' => '3.6.0-0.0.0',
-		),
-		'conflicts' =>
-		array (
-		),
-		'suggests' =>
-		array (
-		),
-	),
-);
-
+    'category'           => 'plugin',
+    'shy'                => 0,
+    'version'            => '7.6.4',
+    'dependencies'       => '',
+    'conflicts'          => '',
+    'priority'           => '',
+    'loadOrder'          => '',
+    'module'             => 'mod1',
+    'state'              => 'stable',
+    'uploadfolder'       => 1,
+    'createDirs'         => 'uploads/tx_abdownloads/categoryImages,uploads/tx_abdownloads/downloadImages,uploads/tx_abdownloads/files',
+    'modify_tables'      => 'be_groups,be_users',
+    'clearcacheonload'   => 1,
+    'lockType'           => '',
+    'author'             => 'Andreas Bulling',
+    'author_email'       => 'typo3@andreas-bulling.de',
+    'author_company'     => '',
+    'CGLcompliance'      => null,
+    'CGLcompliance_note' => null,
+    'constraints'        =>
+        [
+            'depends'   =>
+                [
+                    'cms'                => '',
+                    'static_info_tables' => '2.0.0',
+                    'php'                => '4.1.0-0.0.0',
+                    'typo3'              => '3.6.0-0.0.0',
+                ],
+            'conflicts' =>
+                [
+                ],
+            'suggests'  =>
+                [
+                ],
+        ],
+];
