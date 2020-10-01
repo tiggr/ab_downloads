5167b9cc9bd41082e778d5c8a3e5491d82c33dfb
Daniel Schöne <daniel@schoene.it>
2020-10-01 14:06:39 +0200

    TCA: fix tt_content list type definition (flexform registration)

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -13,15 +13,18 @@
     'FILE:EXT:ab_downloads/Configuration/FlexForms/flexform_ds.xml'
 );
 
-return ([
-    "types" => [
-        "list" => [
-            'subtypes_excludelist' => [
-                "tx_abdownloads_p1" => 'layout,select_key,pages,recursive',
-            ],
-            'subtypes_addlist'     => [
-                "tx_abdownloads_p1" => 'pi_flexform',
-            ],
-        ],
-    ],
-]);
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['ab_downloads_pi1'] = 'layout,select_key,pages,recursive';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['ab_downloads_pi1'] = 'pi_flexform';
+
+//return ([
+//    "types" => [
+//        "list" => [
+//            'subtypes_excludelist' => [
+//                "tx_abdownloads_p1" => 'layout,select_key,pages,recursive',
+//            ],
+//            'subtypes_addlist'     => [
+//                "tx_abdownloads_p1" => 'pi_flexform',
+//            ],
+//        ],
+//    ],
+//]);
